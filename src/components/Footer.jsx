35c1dc373097679ddfa997771169212c643d8bb3
Boldecca <muteterirebecca60@gmail.com>
2026-05-25 20:26:20 +0200

    updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2024 Rebecca Muteteri. Designed & Built with ❤️</p>
+      <p>Designed & Built by <span>Rebecca Muteteri</span></p>
+      <p className={styles.sub}>© 2024 · All rights reserved</p>
     </footer>
   )
 }
